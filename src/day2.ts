529e45142ce7b9529ef165dd41375bd2c9299853
Michael <sheveksmath@gmail.com>
2019-12-03 21:11:32 -0500

    completed day 2, problem 2

--- src/day2.ts
+++ src/day2.ts
@@ -3,8 +3,8 @@ const intCodes = [
     1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 9, 19, 23, 1, 23, 5, 27, 2, 27, 10, 31, 1, 6, 31, 35, 1, 6, 35, 39, 2, 9, 39, 43, 1, 6, 43, 47, 1, 47, 5, 51, 1, 51, 13, 55, 1, 55, 13, 59, 1, 59, 5, 63, 2, 63, 6, 67, 1, 5, 67, 71, 1, 71, 13, 75, 1, 10, 75, 79, 2, 79, 6, 83, 2, 9, 83, 87, 1, 5, 87, 91, 1, 91, 5, 95, 2, 9, 95, 99, 1, 6, 99, 103, 1, 9, 103, 107, 2, 9, 107, 111, 1, 111, 6, 115, 2, 9, 115, 119, 1, 119, 6, 123, 1, 123, 9, 127, 2, 127, 13, 131, 1, 131, 9, 135, 1, 10, 135, 139, 2, 139, 10, 143, 1, 143, 5, 147, 2, 147, 6, 151, 1, 151, 5, 155, 1, 2, 155, 159, 1, 6, 159, 0, 99, 2, 0, 14, 0
 ]
 
-intCodes[1] = 12;
-intCodes[2] = 2;
+// intCodes[1] = 12;
+// intCodes[2] = 2;
 
 export const processCodes = (intCodes: number[], currentPosition: number = 0): number => {
 
@@ -44,12 +44,33 @@ const performOpcodeOperation = (intCodes: number[], currentPosition: number, op:
 
     let updatedIntCodes = setValue(intCodes, result, resultPosition);
     return updatedIntCodes;
-
 }
 
 const getSum = (a: number, b: number) => a + b;
 const getProduct = (a: number, b: number) => a * b;
 
+const setInputs = (a: number, b: number, intCodes: number[]) => {
+    return [intCodes[0], a, b, ...intCodes.slice(3)]
+}
+
 console.log(
-    processCodes(intCodes)
+    processCodes(setInputs(12, 2, intCodes))
 );
+
+// Problem 2
+// ugly n^2 solution
+
+const arr = Array.from(Array(100).keys());
+
+const getInputsByOutput = (arr: number[], desiredOutput: number) => {
+    arr.forEach(n => {
+        arr.forEach(m => {
+            if (processCodes(setInputs(n, m, intCodes)) === desiredOutput) {
+                console.log(processCodes(setInputs(n, m, intCodes)));
+                console.log(`${n} ${m}`);
+            }
+        })
+    });
+}
+
+getInputsByOutput(arr, 19690720)
